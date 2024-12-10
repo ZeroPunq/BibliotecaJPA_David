08ce0f4d5d7819b83f160a6f3d2773a0a2da075a
ZeroPunq <davidironhell@gmail.com>
2024-12-10 08:52:04 +0100

    Importación de tablas

---
+++ src/main/java/DTOs/Ejemplar.java
@@ -0,0 +1,48 @@
+package DTOs;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.OnDelete;
+import org.hibernate.annotations.OnDeleteAction;
+
+@Entity
+public class Ejemplar {
+    @Id
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @ManyToOne(fetch = FetchType.LAZY, optional = false)
+    @OnDelete(action = OnDeleteAction.CASCADE)
+    @JoinColumn(name = "isbn", nullable = false)
+    private DTOs.Libro isbn;
+
+    @ColumnDefault("'Disponible'")
+    @Lob
+    @Column(name = "estado")
+    private String estado;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public DTOs.Libro getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(DTOs.Libro isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+
+}
